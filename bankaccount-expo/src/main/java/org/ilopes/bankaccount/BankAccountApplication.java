e2c0ea5d540e2126ca587cdcd075eac0639086b7
Idalecio Lopes <idalecio.lopes@gmail.com>
2020-07-08 13:50:25 +0200

    :sparkles: #US3 check my operations delivered

--- bankaccount-expo/src/main/java/org/ilopes/bankaccount/BankAccountApplication.java
+++ bankaccount-expo/src/main/java/org/ilopes/bankaccount/BankAccountApplication.java
@@ -2,8 +2,10 @@ package org.ilopes.bankaccount;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@SpringBootApplication(scanBasePackages = {"org.ilopes.bankaccount", "org.ilopes.bankaccount.personalaccount","org.ilopes.bankaccount.rest"})
+@SpringBootApplication(scanBasePackages = {"org.ilopes.bankaccount"})
+@EnableSwagger2
 public class BankAccountApplication {
     public static void main(String[] args) {
         SpringApplication.run(BankAccountApplication.class, args);
